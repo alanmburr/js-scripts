2bd557a9240e5dded0b35d13205a94e40a0bbbbb
Alan Burr <wackyblackie-page-feedback@outlook.com>
2022-03-26 09:45:53 -0400

    Add Ukraine Colors

---
+++ ua-colors.js
@@ -0,0 +1,21 @@
+/*
+Copyright (C) 2022  wackyblackie
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+var html = document.getElementsByTagName('html')[0];
+html.style.height = '100%';
+html.style.width = '100%';
+html.style.backgroundImage = 'linear-gradient(rgba(0,87,184,0.1),rgba(255,215,0,0.1))';
+html.style.backgroundSize = 'cover';
+html.style.backgroundPosition = 'center';
+html.style.backgroundRepeat = 'no-repeat';
+html.style.backgroundAttachment = 'fixed';
---
+++ ua-colors.md
@@ -0,0 +1,17 @@
+---
+title: ua-colors.js
+permalink: /js-scripts/ua-colors.md
+---
+Usage:
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    ...
+    <script src="ua-colors.js" integrity="sha384-6dD3ajnO2UZivJuTKybUkiexPy8Tjen343+PMwMo3fh3uUcqCjuNHfQ1jPAbJltj"></script>
+</head>
+<body>...</body>
+</html>
+```
+
+Production ready, but may break some css templates, such as Bootstrap.
